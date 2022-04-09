5ff2dcfcf0d3eb175f0d9893644a114da1d7cb15
Odbayar <odbayar.0427@yahoo.com>
2022-04-09 19:41:27 +0800

    New game - ийг ажиллуулсан

--- app.js
+++ app.js
@@ -1,26 +1,58 @@
-// Тоглогчдын ээлжийг хадгалах хувьсагч. Нэгдүгээр тогчлогч - 0, хоёрдугаар тоглогч - 1
-var activePlayer = 0;
+// // Тоглогчдын ээлжийг хадгалах хувьсагч. Нэгдүгээр тогчлогч - 0, хоёрдугаар тоглогч - 1
+var activePlayer;
 
-// Тоглогчдын оноог цуглуулах хувьсагч
-var scores = [0, 0];
+// // Тоглогчдын оноог цуглуулах хувьсагч
+var scores;
 
-// Ээлжийн оноог хадгалах хувьсагч
-var roundScore = 0;
+// // Ээлжийн оноог хадгалах хувьсагч
+var roundScore;
 
-// Эхний тоглогч
-var PlayerOneScore = document.getElementById("score-0");
-PlayerOneScore.textContent = 0;
-var PlayerOneCurrentScore = document.getElementById("current-0");
-PlayerOneCurrentScore.textContent = 0;
-
-// Хоёр дахь тоглогч
-var PlayerTwoScore = document.getElementById("score-1");
-PlayerTwoScore.textContent = 0;
-var PlayerTwoCurrentScore = document.getElementById("current-1");
-PlayerTwoCurrentScore.textContent = 0;
-
-// Эхлэх үед
 var diceDom = document.querySelector(".dice");
+
+newGame();
+
+function newGame() {
+  // Тоглогчдын ээлжийг хадгалах хувьсагч. Нэгдүгээр тогчлогч - 0, хоёрдугаар тоглогч - 1
+  activePlayer = 0;
+
+  // Тоглогчдын оноог цуглуулах хувьсагч
+  scores = [0, 0];
+
+  // Ээлжийн оноог хадгалах хувьсагч
+  roundScore = 0;
+
+  // Эхний тоглогч
+  var PlayerOneScore = document.getElementById("score-0");
+  PlayerOneScore.textContent = 0;
+  var PlayerOneCurrentScore = document.getElementById("current-0");
+  PlayerOneCurrentScore.textContent = 0;
+
+  // Хоёр дахь тоглогч
+  var PlayerTwoScore = document.getElementById("score-1");
+  PlayerTwoScore.textContent = 0;
+  var PlayerTwoCurrentScore = document.getElementById("current-1");
+  PlayerTwoCurrentScore.textContent = 0;
+
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+
+  document.querySelector(".player-0-panel").classList.add("active");
+
+  // ROLL болон HOLD button-уудыг гаргаж ирч байна
+  document.querySelector(".btn-hold").style.display = "block";
+  document.querySelector(".btn-roll").style.display = "block";
+
+  // DICE-ийг алга болгож байна
+  diceDom.style.display = "none";
+}
+
+////////////////////////////////////////////// Эхлэх үед //////////////////////////////////////////////
 
 // Шоог шидэх үед зураг нь солигдоно. Бас оноо нэмэгдэнэ
 document.querySelector(".btn-roll").addEventListener("click", function () {
@@ -48,7 +80,7 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
   }
 });
 
-// Hold button дарагдах үед
+////////////////////////////////////////////// Hold button дарагдах үед //////////////////////////////////////////////
 document.querySelector(".btn-hold").addEventListener("click", function () {
   // Current Score-ыг идэвхитэй тоглогчийн онооруу нэмэх
   scores[activePlayer] = scores[activePlayer] + roundScore;
@@ -58,18 +90,28 @@ document.querySelector(".btn-hold").addEventListener("click", function () {
     scores[activePlayer];
 
   if (scores[activePlayer] >= 10) {
-    //
+    //Хожсон тоглогчийн нэрийг WINNER болгож байна
     document.getElementById("name-" + activePlayer).textContent = "WINNER";
 
+    // Хожсон тоглогчийн нэрийг CSS-ээс winner гэсэн class-тай холбож өгч байна
     document
       .querySelector(".player-" + activePlayer + "-panel")
       .classList.add("winner");
-  } else {
+
+    // ROLL болон HOLD button-уудыг алга болгож байна
+    document.querySelector(".btn-hold").style.display = "none";
+    document.querySelector(".btn-roll").style.display = "none";
+  }
+  // Хэрвээ хэн нэг нь хожоогүй бол nextPlayer функцийг дуудсаар байх хэрэгтэй
+  else {
     nextPlayer();
   }
 
   //Дараачийн тоглогчруу шилжинэ
 });
+
+////////////////////////////////////// Шинээр хэлэх //////////////////////////////////////////////
+document.querySelector(".btn-new").addEventListener("click", newGame);
 
 function nextPlayer() {
   // Шооны зургыг алга болгох
